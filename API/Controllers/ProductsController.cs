aa8427bb60df80ae9c37eea31dd36d9654b11f63
pedramsaharkhiz <pedram_saharkhiz@yahoo.com>
2023-11-11 13:29:19 +0330

    Add Migrations and refactor the project to Data and Infrastructure folder

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,16 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class ProductsController : BaseAPIController
     {
-        [HttpGet("{quantity}")]
-        public async Task<int> GetProductCount(int quantity)
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return  quantity*2;
+            _context = context;
+            
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<ActionResult<IEnumerable<Product>>>GetAllProducts()
+        {
+            var response=await _context.Products.ToListAsync();
+            if(response.Any())return Ok(response);
+            return NotFound("No Products Found!");
+        }
+        [HttpGet("GetProductById/{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var response=await _context.Products.FindAsync(id);
+            if(response !=null)return Ok(response);
+            return NotFound($"Response with id {id} is not found!");
         }
     }
 }
